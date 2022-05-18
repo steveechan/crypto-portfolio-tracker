32aa5acfe24f9890c3a370035b1e94beb7d0396a
stephen <chanstephen2121@gmail.com>
2022-05-18 15:27:31 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="app">
+  <div class="entry">
+  Token <input type="text" required v-model="token.name">
+  Price <input type="text" required v-model="token.price">
+  Quantity <input type="text" required v-model="token.quantity">
+  <button @click="addData"> Add </button>
+  {{ tokens }}
+  </div>
+  <div class="portfolio">
+    <table class="styled-table">
+    <thead>
+        <tr>
+            <th>Token</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total Value</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="token in tokens" :key="token.item">
+            <td> {{ token.name }} </td>
+            <td> {{ token.price }} </td>
+            <td> {{ token.quantity }} </td>
+        </tr>
+            
+    </tbody>
+</table>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      tokens: [],
+      
+      token: {
+        name: "",
+        price: "",
+        quantity: "",
+      }
+    }
+  },
+  methods: {
+    addData() {
+      this.tokens.push( {
+        name: this.token.name,
+        price: this.token.price,
+        quantity: this.token.quantity,
+      })
+      this.token.name = "";
+      this.token.price = "";
+      this.token.quantity = "";
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Add.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="app">
+  <div class="entry">
+  Token <input type="text" required v-model="token.name">
+  Price <input type="text" required v-model="token.price">
+  Quantity <input type="text" required v-model="token.quantity">
+  <button @click="addData"> Add </button>
+  {{ tokens }}
+  </div>
+  <div class="portfolio">
+      <td> Token </td>
+      <ul class="list">
+        <li v-for="token in tokens" :key="token.name"> {{ token.name }}  </li>
+      </ul>
+      <td> Shares </td>
+      <ul class="list">
+        <li v-for="token in tokens" :key="token.price"> {{ token.price }}  </li>
+      </ul>
+      <td> Quantity </td>
+      <ul class="list">
+        <li v-for="token in tokens" :key="token.quantity"> {{ token.quantity }}  </li>
+      </ul>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: "Add-Component",
+  data () {
+    return {
+      tokens: [],
+      
+      token: {
+        name: "",
+        price: "",
+        quantity: "",
+      }
+    }
+  },
+  methods: {
+    addData() {
+      this.tokens.push(this.token)
+      this.name = ""
+    }
+  }
+}
+</script>
+
+<style>
+.list {
+  list-style: none;
+}
+</style>
