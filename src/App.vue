532e98de96f6396fb452478c18d9923cd423259b
stephen <chanstephen2121@gmail.com>
2022-05-19 17:49:38 -0400

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
 <div class="app">
-  <div class="entry">
-  Token <select required v-model="token.name"> 
-          <option v-for="coin in coins" :key="coin.item"> {{ coin.symbol }} </option>
-        </select> <br>
-  Price <input type="text" required v-model="token.price"> <br>
-  Quantity <input type="text" required v-model="token.quantity"> <br>
-  <button @click="addData"> Add </button>
+  <div v-if="showModal" class="modal-mask">
+    <div class="entry">
+    Token <select required v-model="token.name"> 
+            <option v-for="coin in coins" :key="coin.item"> {{ coin.symbol }} </option>
+          </select> <br>
+    Price <input type="number" required v-model="token.price"> <br>
+    Quantity <input type="number" required v-model="token.quantity"> <br>
+    <button @click="addData"> Add </button>
+    </div>
   </div>
   <div class="portfolio">
     <table class="styled-table">
@@ -19,6 +21,7 @@
             <th>Total Investment</th>
             <th>Current Investment</th>
             <th>Current Price</th>
+            <th><button v-if="showPlus" @click="showAdd"> + </button></th>
         </tr>
     </thead>
     <tbody>
@@ -29,8 +32,9 @@
             <td> {{ token.quantity }} </td>
             <td> {{ "$" + token.totalVal }} </td>
             <td> {{ "$" + parseFloat(token.curr.current_price) * token.quantity }} </td>
-            <td> {{ "$" + token.curr.current_price }} <button @click="removeData"> - </button></td>
+            <td> {{ "$" + token.curr.current_price }} <button @click="removeData" class="minus"> - </button></td>
         </tr>
+
             
     </tbody>
     </table>
@@ -60,6 +64,10 @@ export default {
       coins: [],
 
       image: false,
+
+      showModal: false,
+      showPlus: true,
+
     }
   },
 
@@ -68,6 +76,10 @@ export default {
       const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h");
       const data = await response.json();
       this.coins = this.coins.concat(data);
+    },
+
+    showAdd() {
+      this.showModal = true;
     },
 
     addData() {
@@ -84,6 +96,8 @@ export default {
       this.token.quantity = "";
       this.curr = "";
       this.image = true;
+      this.showPlus = true;
+      this.showModal = false;
     },
 
     removeData(index) {
@@ -99,6 +113,24 @@ export default {
   font-family: 'Poppins', sans-serif;
 }
 
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.entry {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
 .portfolio {
   display: flex;
   align-items: center;
@@ -145,6 +177,10 @@ export default {
   width: 20px;
   height: 20px;
   display: block;
+}
+
+.minus {
+  float: right;
 }
 
 </style>
