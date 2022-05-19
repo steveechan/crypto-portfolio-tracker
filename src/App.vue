5d6a815c1504f3d99cc887ba51f1df98a10b4f6d
stephen <chanstephen2121@gmail.com>
2022-05-19 16:19:03 -0400

    images, styling

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,16 @@
   <div class="entry">
   Token <select required v-model="token.name"> 
           <option v-for="coin in coins" :key="coin.item"> {{ coin.symbol }} </option>
-        </select>
-  Price <input type="text" required v-model="token.price">
-  Quantity <input type="text" required v-model="token.quantity">
+        </select> <br>
+  Price <input type="text" required v-model="token.price"> <br>
+  Quantity <input type="text" required v-model="token.quantity"> <br>
   <button @click="addData"> Add </button>
   </div>
   <div class="portfolio">
-    <table>
+    <table class="styled-table">
     <thead>
         <tr>
+            <th v-if="image">  </th>
             <th>Token</th>
             <th>Price</th>
             <th>Quantity</th>
@@ -21,13 +22,14 @@
         </tr>
     </thead>
     <tbody>
-        <tr v-for="token in tokens" :key="token.name">
+        <tr class="active-row" v-for="(token, index) in tokens" :key="index">
+            <td> <img :src="token.curr.image" class="tokenimage" /> </td>
             <td> {{ token.name.toUpperCase() }} </td>
             <td> {{ "$" + token.price }} </td>
             <td> {{ token.quantity }} </td>
             <td> {{ "$" + token.totalVal }} </td>
             <td> {{ "$" + parseFloat(token.curr.current_price) * token.quantity }} </td>
-            <td> {{ "$" + token.curr.current_price }}</td>
+            <td> {{ "$" + token.curr.current_price }} <button @click="removeData"> - </button></td>
         </tr>
             
     </tbody>
@@ -51,10 +53,13 @@ export default {
         price: "",
         quantity: "",
         totalVal: 0,
+        totalIn: 0,
         curr: 0,
       },
 
       coins: [],
+
+      image: false,
     }
   },
 
@@ -71,12 +76,18 @@ export default {
         price: this.token.price,
         quantity: this.token.quantity,
         totalVal: parseFloat(this.token.price) * this.token.quantity,
+        totalIn: parseFloat(this.token.curr.current_price) * this.token.quantity,
         curr: this.coins.find(c=>c.symbol === this.token.name),
       })
       this.token.name = "";
       this.token.price = "";
       this.token.quantity = "";
       this.curr = "";
+      this.image = true;
+    },
+
+    removeData(index) {
+      this.tokens.splice(index, 1);
     }
   },
 }
@@ -87,17 +98,53 @@ export default {
 * {
   font-family: 'Poppins', sans-serif;
 }
-table,
-th,
-td {
-  border: 10px solid rgba(187, 169, 169, 0.548);
-  padding: 10px;
-  border-collapse: collapse;
-}
 
 .portfolio {
   display: flex;
   align-items: center;
   justify-content: center;
 }
+
+.styled-table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    min-width: 400px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.styled-table thead tr {
+    background-color: #009879;
+    color: #ffffff;
+    text-align: left;
+}
+
+.styled-table th,
+.styled-table td {
+    padding: 12px 15px;
+}
+
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid #009879;
+}
+
+.styled-table tbody tr.active-row {
+    font-weight: bold;
+    color: #009879;
+}
+
+.tokenimage {
+  width: 20px;
+  height: 20px;
+  display: block;
+}
+
 </style>
