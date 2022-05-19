46913635800ed50d4811a2acc0e21bfbe2494e55
stephen <chanstephen2121@gmail.com>
2022-05-18 21:05:17 -0400

    current price updates, added api calls, total value

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="app">
   <div class="entry">
-  <button @click="fetchCoins">Click it or ticket</button>
-  Token <input type="text" required v-model="token.name">
+  Token <select required v-model="token.name"> 
+          <option v-for="coin in coins" :key="coin.item"> {{ coin.symbol }} </option>
+        </select>
   Price <input type="text" required v-model="token.price">
   Quantity <input type="text" required v-model="token.quantity">
   <button @click="addData"> Add </button>
@@ -19,11 +20,12 @@
         </tr>
     </thead>
     <tbody>
-        <tr v-for="token in tokens" :key="token.item">
-            <td> {{ token.name }} </td>
+        <tr v-for="token in tokens" :key="token.name">
+            <td> {{ token.name.toUpperCase() }} </td>
             <td> {{ "$" + token.price }} </td>
             <td> {{ token.quantity }} </td>
             <td> {{ "$" + token.totalVal }} </td>
+            <td> {{ token.curr.current_price }}</td>
         </tr>
             
     </tbody>
@@ -35,6 +37,9 @@
 <script>
 
 export default {
+  mounted() {
+    this.fetchCoins();
+  },
   data () {
     return {
       tokens: [],
@@ -44,21 +49,18 @@ export default {
         price: "",
         quantity: "",
         totalVal: 0,
+        curr: 0,
       },
 
       coins: [],
     }
   },
 
-  setup() {
-
-  },
   methods: {
     async fetchCoins() {
       const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h");
       const data = await response.json();
-      console.log("new coins", data);
-      this.coins.push(data);
+      this.coins = this.coins.concat(data);
     },
 
     addData() {
@@ -67,15 +69,20 @@ export default {
         price: this.token.price,
         quantity: this.token.quantity,
         totalVal: parseFloat(this.token.price) * this.token.quantity,
+        curr: this.coins.find(c=>c.symbol === this.token.name),
       })
+      console.log(this.curr);
       this.token.name = "";
       this.token.price = "";
       this.token.quantity = "";
+      this.curr = "";
     }
-  }
+  },
 }
 </script>
 
 <style>
-
+td {
+  text-align: center;
+}
 </style>
